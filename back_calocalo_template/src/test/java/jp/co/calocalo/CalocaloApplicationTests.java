fdec01048884734455af60ad84346579e0ff7ff6
kojimakazuya <kojima-k@softbrain.co.jp>
2022-04-28 11:58:23 +0900

    add template

---
+++ back_calocalo_template/src/test/java/jp/co/calocalo/CalocaloApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.calocalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalocaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
