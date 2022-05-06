5bb9263edc294dfa4214cc7bbfa4141deae36371
湯川 <yukawa@softbrain.co.jp>
2022-05-06 14:22:18 +0900

    FoodListController

--- calocalo/src/main/java/jp/co/calocalo/controller/FoodListController.java
+++ calocalo/src/main/java/jp/co/calocalo/controller/FoodListController.java
@@ -1,6 +1,27 @@
 package jp.co.calocalo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jp.co.calocalo.entity.FoodListEntity;
+import jp.co.calocalo.repository.FoodListRepository;
+
+@RestController
+@RequestMapping("/food_list")
 public class FoodListController {
 	
+	@Autowired
+	FoodListRepository foodListRepository;
 	
+	@GetMapping
+	public List<FoodListEntity> findFoodList() {
+		
+		List<FoodListEntity> foodList = foodListRepository.findAll();
+		
+		return foodList;
+	}
 }
