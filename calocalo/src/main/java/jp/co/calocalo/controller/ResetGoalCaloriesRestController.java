3797861f8b3f1d06f18f362750c311b2012ae413
湯川 <yukawa@softbrain.co.jp>
2022-05-10 12:00:49 +0900

    実装用にリファクタリング

--- calocalo/src/main/java/jp/co/calocalo/controller/ResetGoalCaloriesRestController.java
+++ calocalo/src/main/java/jp/co/calocalo/controller/ResetGoalCaloriesRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jp.co.calocalo.entity.AdminEntity;
 import jp.co.calocalo.entity.EmployeesJoinAdminEntity;
-import jp.co.calocalo.form.DemoResetForm;
+import jp.co.calocalo.form.GoalCaloUpdateForm;
 import jp.co.calocalo.repository.AdminRepository;
 import jp.co.calocalo.repository.EmployeesJoinAdminRepository;
 
@@ -21,20 +21,18 @@ public class ResetGoalCaloriesRestController {
 	@Autowired EmployeesJoinAdminRepository employeesJoinAdminRepository;
 
 	//カロリーの再設定
-	
 	@PostMapping("/mypage/setting/{emp_id}")
-	public  EmployeesJoinAdminEntity signUp(@PathVariable int emp_id, DemoResetForm form) {
+	public  void signUp(@PathVariable int emp_id, GoalCaloUpdateForm form) {
 		
 		EmployeesJoinAdminEntity reCaloEntity = new EmployeesJoinAdminEntity();
 		AdminEntity admin = adminRepository.getById(1);
 		EmployeesJoinAdminEntity employee = employeesJoinAdminRepository.getById(emp_id);
 
+//		データの再代入からの保存
 		reCaloEntity.setEmp_id(emp_id);
-		reCaloEntity.setGoal_calories(form.getGoal_calories());
+		reCaloEntity.setGoal_calories(form.getGoal_calorie());
 		reCaloEntity.setAdminEntity(admin);
 		reCaloEntity.setPassword(employee.getPassword());
 		employeesAdminRepository.save(reCaloEntity);
-		
-		return reCaloEntity;
 	}
 }
