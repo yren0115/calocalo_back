bfea3a01183abe24f84eae806d2d0d38dffc3ae6
湯川 <yukawa@softbrain.co.jp>
2022-05-10 09:20:47 +0900

    テンプレの修正

--- calocalo/src/main/java/jp/co/calocalo/security/SecurityConfig.java
+++ calocalo/src/main/java/jp/co/calocalo/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package jp.co.calocalo.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Profile("production") // spring securityの無効化
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
